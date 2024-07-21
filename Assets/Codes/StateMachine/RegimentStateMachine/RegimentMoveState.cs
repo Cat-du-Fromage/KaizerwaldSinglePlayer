3ef3b1dc673a4580d53c3c23cd7bd5710b4b47d4
Florian <florian.duruz@gmail.com>
2024-07-21 20:49:56 +0200

    Update RegimentMoveState.cs

--- Assets/Codes/StateMachine/RegimentStateMachine/RegimentMoveState.cs
+++ Assets/Codes/StateMachine/RegimentStateMachine/RegimentMoveState.cs
@@ -80,10 +80,9 @@ namespace Kaizerwald.StateMachine
         public override void OnSetup(Order order)
         {
             //Setup to initial rotation and forward position
-            float3 center = Position - CurrentFormation.DirectionForward * (CurrentFormation.DistanceUnitToUnit.y * (CurrentFormation.Depth - 1));
-            float diameter = cmax(float2(CurrentFormation.WidthDepth - 1) * CurrentFormation.DistanceUnitToUnit);
-
-            Vector3 startPosition = center + center.direction(order.TargetPosition) * (diameter / 2f);
+            //Vector3 startPosition = GetStartPosition2(order);
+            
+            float3 startPosition = GetStartPosition(order).x0y();
             Quaternion startRotation = Quaternion.LookRotation(order.TargetFormation.Direction3DForward, Vector3.up);
             RegimentTransform.SetPositionAndRotation(startPosition, startRotation);
             
@@ -154,18 +153,73 @@ namespace Kaizerwald.StateMachine
         }
         
     //╓────────────────────────────────────────────────────────────────────────────────────────────────────────────────╖
+    //║ ◈◈◈◈◈◈ On Setup ◈◈◈◈◈◈                                                                                         ║
+    //╙────────────────────────────────────────────────────────────────────────────────────────────────────────────────╜
+
+        private Vector3 GetStartPosition2(Order order)
+        {
+            float3 center = Position - CurrentFormation.DirectionForward * (CurrentFormation.DistanceUnitToUnit.y * (CurrentFormation.Depth - 1));
+            float diameter = cmax(float2(CurrentFormation.WidthDepth - 1) * CurrentFormation.DistanceUnitToUnit);
+            return center + center.direction(order.TargetPosition) * (diameter / 2f);
+        }
+    
+        private int GetClosestUnitIndexFromTarget(float2 target)
+        {
+            int count = CurrentFormation.UnitCount;
+            int halfCount = count / 2;
+            (int minIndex, float minDistance) = (halfCount, distance(LinkedRegiment[halfCount].Position.xz, target));
+            (int minStartEndIndex, float minDistanceStartEnd) = (0, float.MaxValue);
+            for (int i = 0; i < halfCount; i++)
+            {
+                float distanceStart = distance(LinkedRegiment[i].Position.xz, target);
+                float distanceEnd   = distance(LinkedRegiment[^(1+i)].Position.xz, target);
+                
+                bool isStartMin = distanceStart < distanceEnd;
+                (minStartEndIndex, minDistanceStartEnd) = isStartMin ? (i, distanceStart) : (count - (1 + i), distanceEnd);
+                bool swapMin = minDistanceStartEnd < minDistance;
+                (minIndex, minDistance) = swapMin ? (minStartEndIndex, minDistanceStartEnd) : (minIndex, minDistance);
+            }
+            return minIndex;
+        }
+        
+        private int GetClosestUnitIndexFromTarget2(float2 target)
+        {
+            (int minIndex, float minDistance) = (0, distance(LinkedRegiment[0].Position.xz,target));
+            for (int i = 1; i < CurrentFormation.UnitCount; i++)
+            {
+                float distance = LinkedRegiment[i].Position.xz.distance(target);
+                (minIndex, minDistance) = distance < minDistance ? (i, distance) : (minIndex, minDistance);
+            }
+            return minIndex;
+        }
+    
+        private float2 GetStartPosition(Order order)
+        {
+            float2 position2D = Position.xz;
+            
+            int closestUnitIndex = GetClosestUnitIndexFromTarget(order.TargetPosition.xz);
+            float2 unitPosition = LinkedRegiment[closestUnitIndex].Position.xz;
+            
+            //2) Segment position to target
+            float2 positionToTarget = order.TargetPosition.xz - position2D;
+            
+            //3) Projection Closest Unit To Segment
+            float2 positionToUnit = unitPosition - position2D;
+            return position2D + project(positionToUnit, positionToTarget);
+        }
+        
+    //╓────────────────────────────────────────────────────────────────────────────────────────────────────────────────╖
     //║ ◈◈◈◈◈◈ On Enter ◈◈◈◈◈◈                                                                                         ║
     //╙────────────────────────────────────────────────────────────────────────────────────────────────────────────────╜
         private NativeArray<float> GetCostMatrix(in FormationData targetFormation)
         {
-
+            int unitCount = targetFormation.UnitCount;
             NativeArray<float3> destinations = ((Formation)targetFormation).GetPositions(LeaderTargetPosition, Temp);
-            NativeArray<float> nativeCostMatrix = new (square(targetFormation.UnitCount), TempJob, UninitializedMemory);
+            NativeArray<float> nativeCostMatrix = new (square(unitCount), TempJob, UninitializedMemory);
             for (int i = 0; i < nativeCostMatrix.Length; i++)
             {
-                (int x, int y) = KzwGrid.GetXY(i, targetFormation.UnitCount);
-                //int y = i / targetFormation.NumUnitsAlive;
-                //int x = i - y * targetFormation.NumUnitsAlive;
+                int y = i / unitCount;
+                int x = i - y * unitCount;
                 float3 unitPosition = LinkedRegiment[y].Position;
                 nativeCostMatrix[i] = distancesq(unitPosition, destinations[x]);
             }
